58abb5e1118a8c0715aee5fe7f47943ef488b77d
GanjiKushal <kushalganjiganji@gmail.com>
2023-04-22 00:34:35 +0530

    added serach functinality and footer

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,9 +7,37 @@ const Body = () => {
   //let [listOfRestaurants, setListOfRestaurants] = arr
   /*-------------------------or-------------------------------------*/
   let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  let [searchText, setSearchTest] = useState("");
+
+  const filterData = (searchText, listOfRestaurants) => {
+    return listOfRestaurants.filter((restaurant) =>
+      restaurant.data.name.includes(searchText)
+    );
+  };
 
   return (
     <div className="body">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchTest(e.target.value);
+          }}
+        />
+
+        <button
+          className="search-button"
+          onClick={() => {
+            const data = filterData(searchText, listOfRestaurants);
+            setListOfRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="filter">
         <button
           className="filter-btn"
