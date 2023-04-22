e3729049bc961e303a619f19a65d71654b0373e1
GanjiKushal <kushalganjiganji@gmail.com>
2023-04-23 00:23:11 +0530

    taking data from swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   //let arr=useState(resList)
   //let [listOfRestaurants, setListOfRestaurants] = arr
   /*-------------------------or-------------------------------------*/
-  let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  let [listOfRestaurants, setListOfRestaurants] = useState([]);
   let [searchText, setSearchTest] = useState("");
 
-  const filterData = (searchText, listOfRestaurants) => {
-    return listOfRestaurants.filter((restaurant) =>
+  const filterData = (searchText, resList) => {
+    return resList.filter((restaurant) =>
       restaurant.data.name.includes(searchText)
     );
   };
+  
+  useEffect(()=>{
+    getRestaurants()
+  },[])
+
+  async function getRestaurants(){
+    const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING")
+    const json=await data.json()
+    console.log(json)
+    setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+  }
 
   return (
     <div className="body">
@@ -31,13 +42,14 @@ const Body = () => {
         <button
           className="search-button"
           onClick={() => {
-            const data = filterData(searchText, listOfRestaurants);
+            const data = filterData(searchText, resList);
             setListOfRestaurants(data);
           }}
         >
           Search
         </button>
       </div>
+     
       <div className="filter">
         <button
           className="filter-btn"
@@ -51,6 +63,7 @@ const Body = () => {
           Get Top Restaurants
         </button>
       </div>
+      
       <div className="resto-container">
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";  //useStatd is a named import
 import { LOGO_URL } from "../utils/constants";
 
 export const Header = () => {
